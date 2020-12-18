9e1348420801230ed0b9e0ce2c976df62f0469f8
Alejandro Vargas <alejandro.vargasmadrid@gmail.com>
2020-12-18 10:37:35 +0100

    created a fizzbuzz challenge simple page

---
+++ fizzbuzz.js
@@ -0,0 +1,23 @@
+// using a loop
+function FizzBuzzA(value1, value2) {
+  let returnValue = '';
+  for(let i = 1; i < 100; i++){
+    if(i%value1 == 0 && i%value2 == 0) {
+      returnValue += 'FizzBuzz ';
+    } else if (i%value1 == 0){
+      returnValue += 'Fizz ';
+    } else if (i%value2 == 0) {
+      returnValue += 'Buzz ';
+    } else {
+      returnValue += i + ' ';
+    }
+  }
+  return returnValue;
+}
+
+// Call and use fizzbuzz
+
+function buzzIt() {
+  let output = '';
+}
+
